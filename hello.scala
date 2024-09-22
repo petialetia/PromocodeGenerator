30ee453bbe7b58011516fb826f997816e96be73d
petialetia <petialetia@yandex.ru>
2024-09-23 02:51:40 +0300

    Hello server is written

--- hello.scala
+++ hello.scala
@@ -2,10 +2,17 @@
 //> using dep com.softwaremill.sttp.tapir::tapir-netty-server-sync:1.11.4
 
 import sttp.tapir.*
+import sttp.tapir.server.netty.sync.NettySyncServer
 
 @main def helloTapir(): Unit =
   val helloEndpoint = endpoint
     .get
     .in("hello" / "world")
+    .in(query[String]("name"))
+    .out(stringBody)
+    .handleSuccess(name => s"Hello, $name!")
 
-  println(helloEndpoint.show)
+  NettySyncServer()
+    .addEndpoint(helloEndpoint)
+    .port(8080)
+    .startAndWait()
