60f37658fd87a02622fa5576382181bc96d039fb
petialetia <petialetia@yandex.ru>
2024-09-23 03:45:45 +0300

    example with swagger docs is implemented

--- hello.scala
+++ hello.scala
@@ -1,18 +1,34 @@
 //> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.4
 //> using dep com.softwaremill.sttp.tapir::tapir-netty-server-sync:1.11.4
+//> using dep com.softwaremill.sttp.tapir::tapir-swagger-ui-bundle:1.11.4
 
+import sttp.shared.Identity
 import sttp.tapir.*
 import sttp.tapir.server.netty.sync.NettySyncServer
+import sttp.tapir.swagger.bundle.SwaggerInterpreter
 
-@main def helloTapir(): Unit =
-  val helloEndpoint = endpoint
+@main def tapirDocs(): Unit =
+  val e1 = endpoint
     .get
     .in("hello" / "world")
     .in(query[String]("name"))
     .out(stringBody)
     .handleSuccess(name => s"Hello, $name!")
 
-  NettySyncServer()
-    .addEndpoint(helloEndpoint)
-    .port(8080)
+  val e2 = endpoint
+    .post
+    .in("double")
+    .in(stringBody)
+    .out(stringBody)
+    .errorOut(stringBody)
+    .handle { s =>
+      s.toIntOption.fold(Left(s"$s is not a number"))(n => Right((n*2).toString))
+    }
+
+  val swaggerEndpoints = SwaggerInterpreter()
+    .fromServerEndpoints[Identity](List(e1, e2), "My App", "1.0")
+
+  NettySyncServer().port(8080)
+    .addEndpoints(List(e1, e2))
+    .addEndpoints(swaggerEndpoints)
     .startAndWait()
