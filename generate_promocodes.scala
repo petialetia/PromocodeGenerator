8b51ec75a0c002e78d4ccab9e23a167984e63695
petialetia <petialetia@yandex.ru>
2024-09-24 06:02:49 +0300

    checkingStatusQueryEndoint template is added, general Error type is added

--- generate_promocodes.scala
+++ generate_promocodes.scala
@@ -17,6 +17,8 @@ import com.github.plokhotnyuk.jsoniter_scala.core.*
 import com.github.plokhotnyuk.jsoniter_scala.macros.*
 import sttp.tapir.json.jsoniter.*
 
+case class Error(description: String) derives ConfiguredJsonValueCodec, Schema
+
 case class GenerationQueryInput(
   n_promocodes: Int,
   n_random_characters: Int,
@@ -24,7 +26,13 @@ case class GenerationQueryInput(
 )
 
 case class GenerationQueryResult(process_id: Int) derives ConfiguredJsonValueCodec, Schema
-case class GenerationQueryError(description: String) derives ConfiguredJsonValueCodec, Schema
+
+type Minutes = Int
+
+case class CheckingStatusQueryResult(
+  progress_persentages: Int,
+  remainig_time_estimation: Option[Minutes]
+) derives ConfiguredJsonValueCodec, Schema
 
 object PromocodeGenerator extends IOApp:
   val generationQueryEndpoint =
@@ -36,7 +44,7 @@ object PromocodeGenerator extends IOApp:
     .in(query[Option[String]]("common_prefix"))
     .mapInTo[GenerationQueryInput]
     .out(jsonBody[GenerationQueryResult])
-    .errorOut(jsonBody[GenerationQueryError])
+    .errorOut(jsonBody[Error])
     .serverLogic[IO](input => IO
       .println(s"Saying hello to: ${input.n_promocodes}")
       .flatMap(_ => IO.pure(Right(GenerationQueryResult(input.n_promocodes)))))
@@ -44,13 +52,27 @@ object PromocodeGenerator extends IOApp:
   val generationQueryRoutes: HttpRoutes[IO] = Http4sServerInterpreter[IO]()
     .toRoutes(generationQueryEndpoint)
 
+  val checkingStatusQueryEndpoint =
+    endpoint
+    .get
+    .in("check_status")
+    .in(query[Int]("process_id"))
+    .out(jsonBody[CheckingStatusQueryResult])
+    .errorOut(jsonBody[Error])
+    .serverLogic[IO](input => IO
+      .println(s"Saying hello to: ${input}")
+      .flatMap(_ => IO.pure(Right(CheckingStatusQueryResult(input, None)))))
+
+  val checkingStatusQueryRoutes: HttpRoutes[IO] = Http4sServerInterpreter[IO]()
+    .toRoutes(checkingStatusQueryEndpoint)
+
   val swaggerEndpoints =
     SwaggerInterpreter()
-    .fromServerEndpoints[IO](List(generationQueryEndpoint), "My App", "1.0")
+    .fromServerEndpoints[IO](List(generationQueryEndpoint, checkingStatusQueryEndpoint), "My App", "1.0")
 
   val swaggerRoutes: HttpRoutes[IO] = Http4sServerInterpreter[IO]().toRoutes(swaggerEndpoints)
 
-  val allRoutes: HttpRoutes[IO] = generationQueryRoutes <+> swaggerRoutes
+  val allRoutes: HttpRoutes[IO] = generationQueryRoutes <+> checkingStatusQueryRoutes <+> swaggerRoutes
 
   override def run(args: List[String]): IO[ExitCode] =
     BlazeServerBuilder[IO]
