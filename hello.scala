4e4d68ea812f3c43d1f9d33cd975a1a04f545392
petialetia <petialetia@yandex.ru>
2024-09-24 04:55:16 +0300

    example server with integration with cats is implemented

--- hello.scala
+++ hello.scala
@@ -1,34 +1,40 @@
-//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.4
-//> using dep com.softwaremill.sttp.tapir::tapir-netty-server-sync:1.11.4
-//> using dep com.softwaremill.sttp.tapir::tapir-swagger-ui-bundle:1.11.4
+//> using dep com.softwaremill.sttp.tapir::tapir-core:1.11.5
+//> using dep com.softwaremill.sttp.tapir::tapir-http4s-server:1.11.5
+//> using dep com.softwaremill.sttp.tapir::tapir-swagger-ui-bundle:1.11.5
+//> using dep org.http4s::http4s-blaze-server:0.23.16
 
-import sttp.shared.Identity
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.syntax.all.*
+import org.http4s.HttpRoutes
+import org.http4s.blaze.server.BlazeServerBuilder
+import org.http4s.server.Router
 import sttp.tapir.*
-import sttp.tapir.server.netty.sync.NettySyncServer
+import sttp.tapir.server.http4s.Http4sServerInterpreter
 import sttp.tapir.swagger.bundle.SwaggerInterpreter
 
-@main def tapirDocs(): Unit =
-  val e1 = endpoint
-    .get
+object HelloWorldTapir extends IOApp:
+  val helloWorldEndpoint = endpoint.get
     .in("hello" / "world")
-    .in(query[String]("name"))
+    .in(query[Int]("name"))
     .out(stringBody)
-    .handleSuccess(name => s"Hello, $name!")
+    .serverLogic[IO](name => IO
+      .println(s"Saying hello to: $name")
+      .flatMap(_ => IO.pure(Right(s"Hello, $name!"))))
 
-  val e2 = endpoint
-    .post
-    .in("double")
-    .in(stringBody)
-    .out(stringBody)
-    .errorOut(stringBody)
-    .handle { s =>
-      s.toIntOption.fold(Left(s"$s is not a number"))(n => Right((n*2).toString))
-    }
+  val helloWorldRoutes: HttpRoutes[IO] = Http4sServerInterpreter[IO]()
+    .toRoutes(helloWorldEndpoint)
 
   val swaggerEndpoints = SwaggerInterpreter()
-    .fromServerEndpoints[Identity](List(e1, e2), "My App", "1.0")
+    .fromServerEndpoints[IO](List(helloWorldEndpoint), "My App", "1.0")
 
-  NettySyncServer().port(8080)
-    .addEndpoints(List(e1, e2))
-    .addEndpoints(swaggerEndpoints)
-    .startAndWait()
+  val swaggerRoutes: HttpRoutes[IO] = Http4sServerInterpreter[IO]().toRoutes(swaggerEndpoints)
+
+  val allRoutes: HttpRoutes[IO] = helloWorldRoutes <+> swaggerRoutes
+
+  override def run(args: List[String]): IO[ExitCode] =
+    BlazeServerBuilder[IO]
+      .bindHttp(8080, "localhost")
+      .withHttpApp(Router("/" -> allRoutes).orNotFound)
+      .resource
+      .use(_ => IO.never)
+      .as(ExitCode.Success)
